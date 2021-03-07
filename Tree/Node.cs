f27ded6d5fdf3309821c53302aa73c3098355a5e
Mike <bjens13@student.sdu.dk>
2021-03-07 15:28:57 +0100

    Initial commit

---
+++ Tree/Node.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Node
+    {
+        // Initial fields for the class
+        public int value;
+        
+        public Node left;
+        public Node right;
+
+        // Constructor
+        public Node(int data)
+        {
+            value = data;
+
+            left = null;
+            right = null;
+        }
+
+        // AddNode method
+        public void AddNode(Node parent)
+        {
+            // If parent node has nothing in it
+            if (parent == null)
+            {
+                Console.WriteLine("Could not call 'AddNode' because parent was 'null'.");
+                return;
+            }
+
+            // If the value of the parent node is the same as the value of the node being added
+            else if (parent.value == this.value)
+            {
+                Console.WriteLine("Duplicate value, nothing has been added");
+                return;
+            }
+
+            // If the value of the node being added is less than the value of the parent node
+            else if (this.value < parent.value)
+            {
+                // Go left in the tree
+                if (parent.left != null)
+                {
+                    // If there is already something in the left node, add a new node
+                    AddNode(parent.left);
+                }
+
+                else
+                {
+                    // Left node is now set as the node being added
+                    Console.WriteLine("Added " + this.value + " to the left of " + parent.value);
+                    parent.left = this;
+                }
+            }
+
+
+            else if (this.value > parent.value)
+            {
+                // Go right in the tree
+                if (parent.right != null)
+                {
+                    // If there is already something in the right node, add a new node
+                    AddNode(parent.right);
+                }
+
+                else
+                {
+                    // Right node is now set as the node being added
+                    Console.WriteLine("Added " + this.value + " to the right of " + parent.value);
+                    parent.right = this;
+                }
+            }
+
+        }
+    }
+}
---
+++ Tree/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Asks the user how many nodes to create and randomly populate.
+             * Also asks what the maximum allowed node value is.
+             * Clears the console after answer.
+             */
+            Console.WriteLine("How many nodes do you want to create and randomly populate? ");
+            Console.Write("Input: ");
+            string inputStringNodes = Console.ReadLine();
+
+            Console.WriteLine("What is the maximum value allowed in the nodes? ");
+            Console.Write("Input: ");
+            string inputStringValue = Console.ReadLine();
+
+            Console.Clear();
+
+            // Converts the inputs to integers
+            int inputIntNode = Convert.ToInt32(inputStringNodes);
+            int inputIntValue = Convert.ToInt32(inputStringValue);
+
+            // Entry node to the tree
+            Node parent = null;
+
+            // Random for populating the tree
+            Random rnd = new Random();
+
+            Console.WriteLine("Creating and populating " + inputStringNodes + " nodes with a maximum node value of " + inputIntValue + ":");
+            Console.WriteLine("====================================================================");
+            Console.WriteLine("");
+
+            // For loop that populates the nodes
+            for (int i = 0; i < inputIntNode; i++)
+            {
+                int randomInt = rnd.Next(0, inputIntValue);
+
+                Node newNode = new Node(randomInt);
+
+                // If the parent node has nothing in it, sets "newNode" as the root of the tree
+                if (parent == null)
+                {
+                    Console.WriteLine(newNode.value + " is now the Root");
+                    parent = newNode;
+                }
+
+                else
+                {
+                    newNode.AddNode(parent);
+                }
+            }
+        }
+    }
+}
